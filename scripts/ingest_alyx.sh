bca1d619e0bf1994c19092dfe1bc86757ca80be0
Chris Turner <cat@199tech.net>
2018-12-20 17:34:18 -0600

    scripts/ingest_alyx.sh: chain stage execution

--- scripts/ingest_alyx.sh
+++ scripts/ingest_alyx.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 cd `dirname $0`
-./fetch_json.sh
-./ingest_alyx_raw.py "$@"
-./ingest_alyx_shadow.py
-./ingest_alyx_shadow_membership.py
-./ingest_alyx_real.py
+./fetch_json.sh \
+&& ./ingest_alyx_raw.py "$@" \
+&& ./ingest_alyx_shadow.py \
+&& ./ingest_alyx_shadow_membership.py \
+&& ./ingest_alyx_real.py
