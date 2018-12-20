3b48a14ca1b76017ecd10961e51b79fc0c88ad43
Chris Turner <cat@199tech.net>
2018-12-20 16:27:47 -0600

    scripts/datajoint-ingest.cron.sh: add wget fetch, document config, etc

--- scripts/datajoint-ingest.cron.sh
+++ scripts/datajoint-ingest.cron.sh
@@ -12,16 +12,39 @@
 #
 #     replaces current user crontab with cronjob to run this script
 #
+# configuration:
+#
+#   The jobs expect:
+# 
+#   - a properly configured dj_local_conf.json in the checkout root to
+#     authenticate to the database
+#
+#   - a properly configured netrc(5) file in the checkout root to authenticate 
+#     the fetch of raw json data, if applicable. 
+#
+#     for reference, netrc file format is as follows:
+#
+#       machine FQDNORIP login LOGINNAME password PASSWORD
+#
+
+host=http://ibl.flatironinstitute.org
+path=json/alyxfull.json.gz
+log=/tmp/datajoint-ingest.log
 
 dosetup(){ crontab ${0%%.sh}; }
 
 dorun() {
 	( cd `dirname $0`/.. \
+		&& echo "# fetching at `date`" \
+			curl -O --netrc-optional=./.netrc ${host}/${path}
+				) >> $log 2>&1
+
+	( cd `dirname $0`/.. \
 		&& echo "# running at `date`" \
 		&& docker-compose \
 			-f docker-compose.yml up \
 			datajoint-ingest \
-				) >> /tmp/datajoint-ingest.log 2>&1
+				) >> $log 2>&1
 }
 
 # _start:
